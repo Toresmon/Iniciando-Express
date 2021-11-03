a2f5fa1150dde659464f09cb79547a996cea170b
Toresmon <etorresherrera@escuelasproa.edu.ar>
2021-11-02 22:06:44 -0300

    Determinante

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const { response } = require('express');
+const response = require('express');
 const express = require('express');
-const { request } = require('http');
+const request = require('http');
 const app = express();
 const port = 5000;
 let precio = 0;
+let determinante = 0;
+
+
 
 app.get('/quepuerto', (request, response) => {
-    response.send(`el puerto utilizado es ${port}`)
-  });
+  response.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
   app.post('/frutasyverduras', (request, response) => {
-    precio+= app.body.precio;
+    for(x of request.body){
+      if(x.tipo == "fruta"){
+          precio += x.precio;
+      }
+    }
     response.send(`el precio de las frutas es de ${precio}`)
+  });
+
+  app.post('/determinante', (req, res) => {
+    determinante = (req.body[0][0]*req.body[1][1])-(req.body[1][0]*req.body[0][1]);
+    res.send(`el determinante de la matriz es: ${determinante}`)
   });
